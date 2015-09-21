5923850acf255f7a539003bb667188b7a0cf4f6c
sonia ramnani <sonia_ramnani@symantec.com>
2015-09-20 19:14:25 -0700

    service to handle data from server

---
+++ client/js/services/carsService.js
@@ -0,0 +1,22 @@
+app.factory('carsService', ['$http', function($http) {
+
+    return {
+        // call to get all cars data
+        get : function() {
+            return $http.get('/api/cars');
+        },
+
+
+        // these will work when more API routes are defined on the Node side of things
+        // call to POST and create a new car
+        create : function(carData) {
+            return $http.post('/api/cars', carData);
+        },
+
+        // call to DELETE a car
+        delete : function(id) {
+            return $http.delete('/api/cars/' + id);
+        }
+    }
+
+}]);
