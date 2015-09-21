935e8497ae29af13ade768173c7f9768ed8b5ba2
sonia ramnani <sonia_ramnani@symantec.com>
2015-09-20 20:47:25 -0700

    getting data from external api for listings and count

--- server.js
+++ server.js
@@ -2,17 +2,39 @@ var express           = require('express'),
     app               = express(),
     bodyParser        = require('body-parser')
     request           = require("request");
+    listingUri        = "http://interview.carlypso.com/listings?offset=10&limit=20";
+    listCountUri      = "http://interview.carlypso.com/count"
+    listCount         = {};
+    listingItems      = {};
 
 app.use(bodyParser());
 
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/client/views/index.html');
-});
+
 
 app.use('/js', express.static(__dirname + '/client/js'));
 app.use('/components', express.static(__dirname + '/client/components'));
+app.use('/css', express.static(__dirname + '/client/css'));
 
 
+request(listingUri, function (error, response) {
+        if (!error && response.statusCode == 200) {
+             listingItems = response.body;
+        }
+    });
+
+request(listCountUri, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        listCount = body;
+    }
+});
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/client/views/index.html');
+
+});
+app.get('/cars',function(req,res){
+
+
+});
 app.listen(9000, function() {
     console.log('I\'m here...');
 })
